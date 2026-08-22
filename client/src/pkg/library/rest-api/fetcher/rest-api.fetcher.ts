c012e8c59f9e226f5a368b78d235d9560473157d
Kamila Lohvynenko <149430081+Kamila-Lohvynenko@users.noreply.github.com>
2026-08-22 17:24:14 +0200

    feat(client): redirect to /login if 401

--- client/src/pkg/library/rest-api/fetcher/rest-api.fetcher.ts
+++ client/src/pkg/library/rest-api/fetcher/rest-api.fetcher.ts
@@ -1,8 +1,30 @@
 import ky, { type KyInstance } from "ky";
 
+import { localizeHref } from "@/paraglide/runtime";
+import { ERoutes } from "@/shared/routes";
+
+const isAuthApiRequest = (url: string): boolean => {
+  return new URL(url).pathname.includes("/auth/");
+};
+
 // fetcher
 export const restApiFetcher: KyInstance = ky.create({
   prefix: process.env.NEXT_PUBLIC_API_URL,
   credentials: "include",
   throwHttpErrors: false,
+  hooks: {
+    afterResponse: [
+      ({ request, response }) => {
+        const isUnauthorized = response.status === 401;
+
+        if (
+          isUnauthorized &&
+          typeof window !== "undefined" &&
+          !isAuthApiRequest(request.url)
+        ) {
+          window.location.href = localizeHref(ERoutes.SIGNIN);
+        }
+      },
+    ],
+  },
 });
